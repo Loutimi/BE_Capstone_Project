d8eaffb795b9e4b5fce2302cb1e2458072998cba
Loutimi <loutimi59@gmail.com>
2024-10-04 17:48:26 +0100

    Modify reviews/urls.py

--- movie_review_api/reviews/urls.py
+++ movie_review_api/reviews/urls.py
@@ -5,16 +5,40 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Create a schema view for Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Movie Review API",
+        default_version='v1',
+        description="API documentation for the Movie Review application",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@moviereview.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Set up the router
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'reviews', ReviewViewSet)
 router.register(r'likes', LikeViewSet)  
 router.register(r'comments', CommentViewSet)
 
+# Define URL patterns
 urlpatterns = [
     path('', include((router.urls, 'api'), namespace='api')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Swagger and ReDoc URLs
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
