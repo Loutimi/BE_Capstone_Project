9921832e3768bfcc4ab3a3a5a201c2826b8cfa2a
Loutimi <loutimi59@gmail.com>
2024-10-04 17:49:35 +0100

    Modify movie_review_api/settings.py

--- movie_review_api/movie_review_api/settings.py
+++ movie_review_api/movie_review_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -61,6 +61,9 @@ REST_FRAMEWORK = {
     ),
 }
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'  # URL to access static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files will be collected
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
